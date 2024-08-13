79bab4996db1b04365545446be6d9f2662c23b9f
Linhtranne <147390264+Linhtranne@users.noreply.github.com>
2024-08-13 10:11:39 +0700

    ss1

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,9 @@ import { faCoffee , faEye , faSave } from "@fortawesome/free-solid-svg-icons";
 export default function page() {
   return (
     <>
-      <h1>bài 3</h1>
       <h2>Tran Quang Linh </h2>
-      <h1>Bài 4</h1>
       <Company />
-      <h1>Bài 5</h1>
       <Button />
-      <h1>Bài 7+8</h1>
-      <h1>bài 10</h1>
       <FontAwesomeIcon icon={faCoffee} />
       <FontAwesomeIcon icon={faEye} />
       <FontAwesomeIcon icon={faSave} />
